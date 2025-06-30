e9e95edc303436b5f280affd9a18be7df3a93d3f
l <bpzj@outlook.com>
2025-06-30 17:51:20 +0800

    update

--- tests/sh600795.rs
+++ tests/sh600795.rs
@@ -2,19 +2,18 @@ use backtest::account::{Account, StockCode};
 use backtest::model::{KLine};
 use backtest::strategy::k_strategy::KStrategy;
 use csv::Reader;
-use egui::debug_text::print;
 
 #[test]
-fn 区间做t() {
+fn mading() {
     // todo 需要先转成前复权数据
-    
+
     // 1. 读取 JSON 文件
     // let file = File::open(r"A:\A\1day\USHA601111.csv").unwrap();
 
     // 2. 解析 JSON 数据
     // let mut bars: Vec<KLine> = serde_json::from_reader(file).unwrap();
     let mut bars = Reader::from_path(r"A:\A\1day\USHA600795.csv").unwrap();
-    
+
     // 3. 初始化账户
     let mut account = Account {
         balance: 1_000_000.0,
